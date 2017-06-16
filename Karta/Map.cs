657f5493e8a48607b7e74509ffc5ca387414d3e0
Patrik Riström <patrik@damn.moe>
2017-06-16 21:12:04 +0200

    Initial commit

---
+++ Karta/Map.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Karta
+{
+    public class Map<T> where T : struct
+    {
+        public T[] values;
+        public readonly int width;
+        public readonly int height;
+
+        public Map(int width, int height, T initialValue) {
+          this.width = width;
+          this.height = height;
+
+          values = new T[width * height];
+          SetAll(initialValue);
+        }
+
+        public T Get(int x, int y) {
+          return values[(y * width) + x];
+        }
+
+        public T? GetSafe(int x, int y) {
+          if (x >= width || x < 0 || y >= height || y < 0) {
+            return null;
+          }
+
+          return values[(y * width) + x];
+        }
+
+        public void Set(int x, int y, T val) {
+          values[(y * width) + x] = val;
+        }
+
+        public void SetAll(T val) {
+          for (int x = 0; x < width; x++)
+          {
+            for (int y = 0; y < height; y++)
+            {
+              Set(x, y, val);
+            }
+          }
+        }
+    }
+}
---
+++ KartaTest/MapTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Karta;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KartaTest {
+    [TestClass]
+    public class MapTest {
+      [TestMethod]
+      public void InitializeValuesArray() {
+        var map = new Map<float>(10, 10, 1f);
+        Assert.AreEqual(100, map.values.Length);
+        Assert.AreEqual(1, map.values[20]);
+      }
+
+      [TestMethod]
+      public void GetShouldGetTheCorrectValue() {
+        var map = new Map<float>(3, 3, 0f);
+        map.values[4] = 2f;
+        Assert.AreEqual(2f, map.Get(1, 1));
+      }
+
+      [TestMethod]
+      public void GetSafeShouldReturnNullIfOutOfBounds() {
+        var map = new Map<float>(3, 3, 0f);
+        map.values[4] = 2f;
+        Assert.AreEqual(map.GetSafe(-1, 0), null);
+        Assert.AreEqual(map.GetSafe(0, -1), null);
+        Assert.AreEqual(map.GetSafe(3, 2), null);
+        Assert.AreEqual(map.GetSafe(2, 3), null);
+      }
+
+      [TestMethod]
+      public void GetSafeShouldReturnCorrectValue() {
+        var map = new Map<float>(3, 3, 0f);
+        map.values[0] = 2f;
+        Assert.AreEqual(map.GetSafe(0, 0), 2f);
+
+        map = new Map<float>(3, 3, 0f);
+        map.values[8] = 2f;
+        Assert.AreEqual(map.GetSafe(2, 2), 2f);
+      }
+
+      [TestMethod]
+      public void SetShouldSetTheValueOfTheCorrectValue() {
+        var map = new Map<float>(3, 3, 0f);
+        map.Set(2, 1, 2f);
+        Assert.AreEqual(2f, map.values[5]);
+      }
+
+      [TestMethod]
+      public void SetAllShouldSetAllValuesToTheSpecifiedValue() {
+        var map = new Map<float>(3, 3, 0f);
+        map.SetAll(5f);
+        Assert.AreEqual(5f, map.values[0]);
+        Assert.AreEqual(5f, map.values[1]);
+        Assert.AreEqual(5f, map.values[2]);
+        Assert.AreEqual(5f, map.values[3]);
+      }
+    }
+}
